3cc70afcb97a6c9745b2881f2108ccb256a6c71f
Onosgb <onosgb@gmail.com>
2023-04-14 10:36:28 +0100

    created NotFound,About,Home pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import NotFound from "./pages/NotFound";
+
 function App() {
   return (
     <div className="App">
-      <h1>Welcome to travel website!</h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
