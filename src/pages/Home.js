e080cacdca5baffbf042a390095d820aefa70ef8
Godbless Onoriode <onosgb@gmail.com>
2023-04-15 13:03:46 +0100

    Home, Header styling with aninmation, created mixins for resuseable purpose

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,15 @@
-import React from "react";
-
+import React, { useState } from "react";
+import Header from '../components/Header'
 const Home = () => {
-  return <div>Home</div>;
+  const [state] = useState({
+    heading: 'We are travel friends',
+    paragraph: 'Come and join us we trave the most famous and beautiful places in the world'
+  })
+  return <>
+    <Header heading={state.heading} paragraph={state.paragraph}> 
+      <button className="btn-default">Get Started</button>
+    </Header>
+  </>;
 };
 
 export default Home;
