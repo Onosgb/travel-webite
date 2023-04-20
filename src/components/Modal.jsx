c1cf199a4d1dc125645574618565c83f00e02d04
Onosgb <onosgb@gmail.com>
2023-04-20 08:48:38 +0100

    created a modal component and used it in home page

---
+++ src/components/Modal.jsx
@@ -0,0 +1,14 @@
+const Modal = () => {
+  return (
+    <div className="modal">
+      <div className="modal__body">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Amet aut nobis
+        voluptates esse accusantium similique quae accusamus officia laborum
+        omnis, temporibus sunt architecto commodi incidunt cum hic eveniet
+        adipisci velit.
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,20 @@
 import React, { useState } from "react";
-import Header from '../components/Header'
+import Header from "../components/Header";
+import Modal from "../components/Modal";
 const Home = () => {
   const [state] = useState({
-    heading: 'We are travel friends',
-    paragraph: 'Come and join us we trave the most famous and beautiful places in the world'
-  })
-  return <>
-    <Header heading={state.heading} paragraph={state.paragraph}> 
-      <button className="btn-default">Get Started</button>
-    </Header>
-  </>;
+    heading: "We are travel friends",
+    paragraph:
+      "Come and join us we trave the most famous and beautiful places in the world",
+  });
+  return (
+    <>
+      <Header heading={state.heading} paragraph={state.paragraph}>
+        <button className="btn-default">Get Started</button>
+      </Header>
+      <Modal />
+    </>
+  );
 };
 
 export default Home;
