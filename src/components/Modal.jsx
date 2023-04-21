96e6120a21f3bb7a3a32e333534a9fbea1595c2c
Onosgb <onosgb@gmail.com>
2023-04-21 12:08:20 +0100

    stopping modal from closing when a user click on modal body

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -5,15 +5,12 @@ const Modal = () => {
   const { state, openModal } = useContext(ModalContext);
 
   return state.modalStatus ? (
-    <div className="modal">
-      <div className="modal__body">
+    <div className="modal" onClick={openModal}>
+      <div className="modal__body" onClick={(e) => e.stopPropagation()}>
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Amet aut nobis
         voluptates esse accusantium similique quae accusamus officia laborum
         omnis, temporibus sunt architecto commodi incidunt cum hic eveniet
         adipisci velit.
-        <button className="btn-default" onClick={openModal}>
-          close
-        </button>
       </div>
     </div>
   ) : (
